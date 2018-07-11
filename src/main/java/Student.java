8b97a53ad2611d192d2f0bb09540685637fdfcc3
wph <617916155@qq.com>
2018-07-11 23:35:53 +0800

    第二次

---
+++ src/main/java/Student.java
@@ -0,0 +1,23 @@
+/**
+ * Created by wph on 2018/7/11.
+ */
+public class Student {
+    private String name;
+    private Integer id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
