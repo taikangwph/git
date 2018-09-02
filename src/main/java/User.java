0e12b6ad5b378bda71991082f79f44436c854117
wph <617916155@qq.com>
2018-09-02 14:05:12 +0800

    第一次提交

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,23 +1,40 @@
+import java.io.Serializable;
+
 /**
  * Created by wph on 2018/7/11.
  */
-public class User {
+public class User implements Serializable{
     private String name;
     private Integer age;
+    private String sex;
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public Integer getAge() {
         return age;
     }
 
+    public String getSex() {
+        return sex;
+    }
+
+    public User(String name, Integer age, String sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
     }
 }
